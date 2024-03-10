d6658ae536fa8cce874fce37c1b6eed26ccd82e9
moryno <mauricenganga41@gmail.com>
2024-03-10 12:40:42 +0300

    Add a carousel for dispaying the category cards

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import "./Home.scss";
 import Featured from "../../components/featured/Featured";
 import Trusted from "../../components/trusted/Trusted";
+import Slide from "../../components/slide/Slide";
+import { cards } from "../../data";
+import CatCard from "../../components/categorycard/CatCard";
 
 const Home = () => {
   return (
     <>
       <Featured />
       <Trusted />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards?.map((card) => (
+          <CatCard key={card?.id} item={card} />
+        ))}
+      </Slide>
     </>
   );
 };
